f9820b147ac0642d232369f2fdfeea98973bb411
Manuel Torres Gil <mtorres@ual.es>
2022-03-08 20:27:38 +0100

    CRUD espacios y solicitudes

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,9 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  urlPersonas: 'http://localhost:3000/personas',
+  urlEspacios: 'http://localhost:3000',
 };
 
 /*
